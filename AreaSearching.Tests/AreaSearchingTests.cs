17567730a843af09003fafb62fc8833330784ab5
Tarveiz <misha28.95@mail.ru>
2023-02-16 17:47:04 +0300

    com_1

---
+++ AreaSearching.Tests/AreaSearchingTests.cs
@@ -0,0 +1,64 @@
+using AreaSearching;
+
+namespace AreaSearching.Tests
+{
+    [TestClass]
+    public class AreaSearchingTests
+    {
+        [TestMethod]
+        public void GetArea_5_25return()
+        {
+            int radius = 5;
+            double expected = 25;
+
+            double actual = AreaSearching.Shared.GetArea(radius);
+
+            Assert.AreEqual(expected, actual);
+        
+        }
+        [TestMethod]
+        public void GetArea_sides3and4and5_angles60and60and60_6return()
+        {
+            List<int> sides = new List<int> { 3, 4, 5 };
+            List<int> angles = new List<int> { 60, 60, 60 };
+            double expected = 6;
+
+            double actual = AreaSearching.Shared.GetArea(sides, angles);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void AppendFigure_Quadrangle3and4_12return()
+        {
+            List<int> sides = new List<int> { 3, 4, 3, 4};
+            List<int> angles = new List<int> { 90, 90, 90, 90 };
+            double expected = 12;
+
+            QuadrangleTests testQuadrangle = new QuadrangleTests();
+            AreaSearching.Shared.AppendFigure(testQuadrangle);
+            double actual = AreaSearching.Shared.GetArea(sides, angles);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    //Просто пример реализации (она плохая, но для примера работы AppendFigure сойдет)
+    public class QuadrangleTests : Figure
+    {
+        public override bool IsThat(List<int>? sides = null, List<int>? angles = null, int? radius = null)
+        {
+            if (sides?.Count == 4 && angles?.Count == 4 && radius == null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        
+
+        public override void CalculateArea(List<int>? sides = null, List<int>? angles = null, int? radius = null)
+        {
+            AreaValue = sides[0] * sides[1];
+        }
+    }
+}
--- AreaSearching/AreaSearching.cs
+++ AreaSearching/AreaSearching.cs
@@ -11,8 +11,8 @@
             {
                 if (currentFigure.IsThat(null, null, radius))
                 {
-                    currentFigure.SetAreaValue(null, null, radius);
-                    return currentFigure.CalculateArea();
+                    currentFigure.CalculateArea(null, null, radius);
+                    return currentFigure.ValidateArea();
                 }
             }
             throw new Exception("No valid figures were found");
@@ -23,20 +23,46 @@
             {
                 if (currentFigure.IsThat(sides, angles, null))
                 {
-                    currentFigure.SetAreaValue(sides, angles, null);
-                    return currentFigure.CalculateArea();
+                    currentFigure.CalculateArea(sides, angles, null);
+                    return currentFigure.ValidateArea();
                 }
             }
             throw new Exception("No valid figures were found");
         }
-
+        /// <summary>
+        /// Добавить фигуру
+        /// </summary>
+        /// <param name="figure"></param>
         public void AppendFigure(Figure figure)
         {
             FigureCheck.Add(figure);
         }
-
-
     }
+    /// <summary>
+    /// Дополнительный функционал к фигурам
+    /// </summary>
+    public class FigureFunction
+    {
+        /// <summary>
+        /// Проверка прямоугольный ли треугольник
+        /// </summary>
+        /// <param name="sides"></param>
+        /// <param name="angles"></param>
+        /// <returns></returns>
+        public bool CheckRightTriangle(List<int> sides, List<int> angles)
+        {
+            if(sides.Count == 3 && angles.Count == 3)
+            {
+                foreach(int angle in angles)
+                {
+                    if (angle == 90)
+                        return true;
+                }
+            }
+        }
+    }
+
+
     /// <summary>
     /// Абстрактный тип фигуры
     /// </summary>
@@ -46,7 +72,7 @@
         /// Значение площади фигуры
         /// </summary>
         public double AreaValue { get; protected set; } = -1;
-        public double CalculateArea()
+        internal double ValidateArea()
         {
             if (AreaValue == -1)
             {
@@ -54,8 +80,23 @@
             }
             return AreaValue;
         }
+        /// <summary>
+        /// Проверка соответствия фигуры
+        /// </summary>
+        /// <param name="sides"></param>
+        /// <param name="angles"></param>
+        /// <param name="radius"></param>
+        /// <returns></returns>
         public abstract bool IsThat(List<int>? sides = null, List<int>? angles = null, int? radius = null);
-        public abstract void SetAreaValue(List<int>? sides = null, List<int>? angles = null, int? radius = null);
+        //public abstract bool IsThat(List<int>? sides = null, List<int>? angles = null);
+
+        /// <summary>
+        /// Подсчет площади фигуры (с присвоением AreaValue)
+        /// </summary>
+        /// <param name="sides"></param>
+        /// <param name="angles"></param>
+        /// <param name="radius"></param>
+        public abstract void CalculateArea(List<int>? sides = null, List<int>? angles = null, int? radius = null);
     }
 
     internal class Circle : Figure
@@ -69,13 +110,9 @@
             return true;
         }
 
-        public override void SetAreaValue(List<int>? sides = null, List<int>? angles = null, int? radius = null)
+        public override void CalculateArea(List<int>? sides = null, List<int>? angles = null, int? radius = null)
         {
-            if (radius.HasValue)
-            {
-                AreaValue = Math.Pow((double)radius, 2);
-            }
-            
+            AreaValue = Math.Pow((double)radius, 2);
         }
     }
 
@@ -83,14 +120,43 @@
     {
         public override bool IsThat(List<int>? sides = null, List<int>? angles = null, int? radius = null)
         {
-            if(sides?.Count == 3 && angles?.Count == 3 && radius == null)
+            bool triangleIsExist()
+            {
+                List<int> sidesAscending = new List<int>();
+                foreach (int i in sides)
+                {
+                    sidesAscending.Add(i);
+                }
+                if (sidesAscending.Min() <= sidesAscending[0])
+                {
+                    int temp = sidesAscending[0];
+                    sidesAscending[0] = sidesAscending.Min();
+                    if (temp <= sidesAscending[1])
+                    {
+                        sidesAscending[2] = sidesAscending[1];
+                        sidesAscending[1] = temp;
+                    }
+                    else if(temp <= sidesAscending[2])
+                    {
+
+                    }
+                }
+
+                for (int i =0; i<sides.Count(); i++)
+                {
+                    if()
+                }
+                
+                
+            }
+            
+            if(sides?.Count == 3 && angles?.Count == 3 && radius == null && angles.Sum() == 180)
             {
                 return true;
             }
             return false;
         }
-
-        public override void SetAreaValue(List<int>? sides = null, List<int>? angles = null, int? radius = null)
+        public override void CalculateArea(List<int>? sides = null, List<int>? angles = null, int? radius = null)
         {
             if(sides != null && angles != null)
             {
